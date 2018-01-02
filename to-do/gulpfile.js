87401e2fd7c0b74a361141f2afc239b3e55bc41f
Joseph Tomlinson and Rane Fields <joseph+rane@>
2018-01-02 15:09:13 -0800

    Add sass (mostly)

---
+++ to-do/gulpfile.js
@@ -0,0 +1,8 @@
+﻿let gulp = require('gulp');
+let sass = require("gulp-sass");
+
+gulp.task("sass", function() {
+  return gulp.src('Styles/master.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./wwwroot/css'));
+});
