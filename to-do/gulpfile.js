23914b73a715a9b9a67b4f18c8ef6fcb81eb9269
Joseph Tomlinson and Rane Fields <joseph+rane@>
2018-01-02 15:42:14 -0800

    Add gulp watch

--- to-do/gulpfile.js
+++ to-do/gulpfile.js
@@ -1,8 +1,30 @@
 ﻿let gulp = require('gulp');
+let del = require('del');
 let sass = require("gulp-sass");
 
-gulp.task("sass", function() {
-  return gulp.src('Styles/master.scss')
+
+// ========== Main Build ========== //
+
+gulp.task('build', ['sass'], function() {
+
+});
+
+
+// ========== Sass ========== //
+
+gulp.task('sass', ['sass:clean'], function() {
+  return gulp.src('Styles/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('./wwwroot/css'));
+});
+
+gulp.task('sass:clean', function() {
+    return del(['./wwwroot/css/*']);
+});
+
+
+// ========== Watch ========== //
+
+gulp.task('watch', function() {
+    gulp.watch(['./Styles/*.scss'], ['sass']);
 });
